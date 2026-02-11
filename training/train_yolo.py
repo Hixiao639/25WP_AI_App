61d6864f0b40d2e1cfa1691b0e4fe62a2f1a9dfd
unknown <jiyunqi2024@sjtu.edu.cn>
2026-02-11 22:22:54 +0100

    feat: modify model training with early stop prevented

--- training/train_yolo.py
+++ training/train_yolo.py
@@ -1,24 +1,16 @@
 from ultralytics import YOLO
 
 def train():
-    # 1. 加载模型 (使用 nano 版本，速度最快，适合 CPU/移动端)
     model = YOLO('yolov8n.pt') 
-
-    # 2. 训练
-    # data: 指向刚才生成的 yaml 文件
-    # epochs: 50 轮通常够用了
-    # imgsz: 对应生成时的图片大小
-    results = model.train(
+    model.train(
         data='math_data.yaml', 
         epochs=50, 
         imgsz=640, 
         batch=16,
-        name='math_yolo_run' # 训练结果保存的文件夹名
+        patience=0,      # 设置为0，禁用早停，强制跑完50轮
+        cache=False,     # 不使用缓存，确保读取的是你新生成的图片
+        name='math_v2'
     )
-    
-    # 3. 验证 (可选)
-    metrics = model.val()
-    print(metrics.box.map)
 
 if __name__ == '__main__':
     train()
